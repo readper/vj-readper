c7a12931cd209674e624ad348affeb42f81eef72
Readper <readper@gmail.com>
2017-10-28 17:23:11 +0800

    add test for PostCreate

---
+++ tests/Feature/PostTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PostTest extends TestCase
+{
+    /**
+     * Post Test creating.
+     *
+     * @return void
+     */
+    public function testPostCreate()
+    {
+        $post = factory(App\Post::class)->create();
+        $testdata = [
+            'title' => 'test title',
+            'author' => 'fake author',
+            'content' => 'test content'
+        ];
+        $this->actingAs($post)
+            ->post('posts', $testdata)
+            ->assertResponseOk()
+            ->seeInDatabase('posts', $testdata);
+    }
+}
